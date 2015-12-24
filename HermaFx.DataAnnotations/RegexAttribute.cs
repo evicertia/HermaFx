040b43c450b8079cfc32ea8c060a0f2b22e65aee
Pablo Ruiz Garcia <pablo.ruiz@gmail.com>
2015-12-24 15:19:31 +0100

    Added RegexAttribute (like RegulaExpressionAttribute, but allowing multiple instances)

---
+++ HermaFx.DataAnnotations/RegexAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace HermaFx.DataAnnotations
+{
+	/// <summary>
+	/// Just like RegularExpressionAttribute, but allowing multiple instances.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = true)]
+	public class RegexAttribute : RegularExpressionAttribute
+	{
+		public RegexAttribute(string pattern)
+			: base(pattern)
+		{
+		}
+	}
+}
