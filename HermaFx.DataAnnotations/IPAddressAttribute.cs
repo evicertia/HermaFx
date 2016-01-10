a23e2f8e93b3922d5534bc1d771c41d4cb2da502
Pablo Ruiz <pablo.ruiz@gmail.com>
2016-01-10 20:26:47 +0100

    Added IPAddressAttribute && NotDefaultAttribute

---
+++ HermaFx.DataAnnotations/IPAddressAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Sockets;
+
+namespace HermaFx.DataAnnotations
+{
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+	public class IPAddressAttribute : ValidationAttribute
+	{
+		private const string _defaultErrorMessage = "Values is not a valid {0} network address";
+		private readonly AddressFamily _family;
+
+		public IPAddressAttribute()
+			: this(AddressFamily.Unknown)
+		{
+		}
+
+		public IPAddressAttribute(AddressFamily family)
+				: base(string.Format(_defaultErrorMessage, family))
+		{
+			_family = family;
+		}
+
+		public override bool IsValid(object value)
+		{
+			IPAddress address = value as IPAddress;
+
+			if (address == null)
+			{
+				var str = Convert.ToString(value);
+				if (!IPAddress.TryParse(str, out address)) return false;
+			}
+
+			if (_family != AddressFamily.Unknown)
+			{
+				return address.AddressFamily == _family;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ HermaFx.DataAnnotations/NotDefaultAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel.DataAnnotations;
+
+using HermaFx;
+
+namespace HermaFx.DataAnnotations
+{
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+	public class NotDefaultAttribute : ValidationAttribute
+	{
+		private const string _defaultErrorMessage = "The field {0} requires a non-default value";
+
+		public NotDefaultAttribute()
+				: base(string.Format(_defaultErrorMessage))
+		{
+		}
+
+		public override bool IsValid(object value)
+		{
+			if (value == null) return true;
+			if (!value.GetType().IsValueType) return true;
+
+			return value == value.GetType().GetDefaultValue();
+		}
+	}
+}
