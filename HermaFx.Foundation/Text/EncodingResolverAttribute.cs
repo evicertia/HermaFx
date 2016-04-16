ebe2adbcd6b7facb1395d3dc4ff15b42e977b3bc
Pablo Ruiz <pablo.ruiz@gmail.com>
2016-04-16 03:47:32 +0200

    Implemented IEncodingResolver into EncodingResolverAttribute

--- HermaFx.Foundation/Text/EncodingResolverAttribute.cs
+++ HermaFx.Foundation/Text/EncodingResolverAttribute.cs
@@ -1,18 +1,15 @@
 ﻿using System;
+using System.Text;
+using System.Linq;
 
 namespace HermaFx.Text
 {
 	[AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
-	sealed class EncodingResolverAttribute : Attribute
+	sealed class EncodingResolverAttribute : Attribute, IEncodingResolver
 	{
-		readonly Type _resolver;
+		private readonly Type _resolver;
+		private readonly Encoding[] _encodings;
 
-		public IEncodingResolver GetResolver()
-		{
-			return (IEncodingResolver)Activator.CreateInstance(_resolver);
-		}
-
-		// This is a positional argument
 		public EncodingResolverAttribute(Type resolver)
 		{
 			Guard.IsNotNull(resolver, nameof(resolver));
@@ -22,6 +19,28 @@ namespace HermaFx.Text
 			);
 
 			_resolver = resolver;
+		}
+
+		public EncodingResolverAttribute(params Type[] encodings)
+		{
+			Guard.IsNotNull(encodings, nameof(encodings));
+			Guard.Against<ArgumentOutOfRangeException>(
+				encodings.Any(x => !typeof(Encoding).IsAssignableFrom(x)),
+				"Some passed objects do not implement {0}", typeof(Encoding).FullName
+			);
+
+			_encodings = encodings.Select(x => (Encoding)Activator.CreateInstance(x)).ToArray();
+		}
+
+		public Encoding GetEncoding(string name)
+		{
+			Guard.IsNotNullNorWhitespace(name, nameof(name));
+			return _encodings.Single(x => x.BodyName.ToLowerInvariant() == name.ToLowerInvariant());
+		}
+
+		public IEncodingResolver GetResolver()
+		{
+			return _encodings != null ? this : (IEncodingResolver)Activator.CreateInstance(_resolver);
 		}
 	}
 }
