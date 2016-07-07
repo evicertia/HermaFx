a46ab164e7f39e7cb256909e5f739e13923c0f3c
Pablo Ruiz <pablo.ruiz@gmail.com>
2016-07-07 18:56:14 +0200

    Added an empty MvcDemo project

---
+++ HermaFx.MvcDemo/App_Start/RouteConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HermaFx.MvcDemo.App_Start
+{
+	public class RouteConfig
+	{
+		public static void Configure(RouteCollection routes)
+		{
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			);
+		}
+	}
+}
