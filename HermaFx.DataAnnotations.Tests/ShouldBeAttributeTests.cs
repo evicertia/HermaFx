5aaa71a7822419cf2089a6aa0c0a99252f87b1cf
Pablo Ruiz <pablo.ruiz@gmail.com>
2016-07-12 17:14:10 +0200

    Added ShouldBeAttribute

---
+++ HermaFx.DataAnnotations.Tests/ShouldBeAttributeTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace HermaFx.DataAnnotations
+{
+	[TestFixture]
+	public class ShouldBeAttributeTests
+	{
+		public class TestClass
+		{
+			[ShouldBe(true)]
+			public bool TrueBoolean { get; set; }
+
+			[ShouldBe(true)]
+			public bool? TrueNullableBoolean { get; set; }
+
+			[ShouldBe(false)]
+			public bool FalseBoolean { get; set; }
+
+			[ShouldBe(false)]
+			public bool? FalseNullableBoolean { get; set; }
+
+			public TestClass()
+			{
+				TrueBoolean = true;
+				TrueNullableBoolean = true;
+
+				FalseBoolean = false;
+				FalseNullableBoolean = false;
+			}
+		}
+
+		[Test]
+		public void Basic_Tests()
+		{
+			var obj = new TestClass();
+			ExtendedValidator.EnsureIsValid(obj);
+		}
+
+		[Test]
+		public void Expect_Failures_When_Values_Do_Not_Match()
+		{
+			var obj = new TestClass()
+			{
+				TrueBoolean = false,
+			};
+			Assert.Throws<AggregateValidationException>(() => ExtendedValidator.EnsureIsValid(obj));
+		}
+
+		[Test]
+		public void Ignore_Null_NotRequired_Properties()
+		{
+			var obj = new TestClass()
+			{
+				TrueNullableBoolean = null,
+				FalseNullableBoolean = null
+			};
+			ExtendedValidator.EnsureIsValid(obj);
+		}
+
+	}
+}
---
+++ HermaFx.DataAnnotations/ShouldBeAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HermaFx.DataAnnotations
+{
+	/// <summary>
+	/// Attribute to ensure a string contains the provided value
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+	sealed public class ShouldBeAttribute : ValidationAttribute
+	{
+		private const string _defaultErrorMessage = "Value should match {0}";
+		readonly object _expected;
+
+		public ShouldBeAttribute(object expected)
+			: base(string.Format(_defaultErrorMessage, expected))
+		{
+			_expected = expected;
+		}
+
+		public override bool IsValid(object value)
+		{
+			// only the [Required] attribute means value can't be null
+			if (value == null) return true;
+
+			// TODO: Try to convert-to/convert-from if value 
+			//		 and _expected types doesn't match.
+			return Object.Equals(value, _expected);
+		}
+	}
+}
