e87d44e5728f5690e87507968d67325bc276f293
David Bueno <dbueno@evicertia.com>
2016-07-27 13:28:59 +0200

    Refactorized. Changed Languages from string to string array.

--- HermaFx.Foundation/Globalization/Country.cs
+++ HermaFx.Foundation/Globalization/Country.cs
@@ -13,38 +13,38 @@ namespace HermaFx.Globalization
 	{
 		#region Fields
 		private Func<string> _name;
-		private Func<string> _official_Name_EN;
+		private Func<string> _officialNameEn;
 		#endregion
 
 		#region Properties
 		/// <summary>
 		/// Alpha-2 codes from ISO 3166-1
 		/// </summary>
-		public string ISO3166_1_Alpha2 { get; private set; }
+		public string Iso2 { get; private set; }
 		/// <summary>
 		/// Alpha-3 codes from ISO 3166-1 (synonymous with World Bank Codes)
 		/// </summary>
-		public string ISO3166_1_Alpha3 { get; private set; }
+		public string Iso3 { get; private set; }
 		/// <summary>
 		/// Numeric codes from ISO 3166-1 (synonymous with UN Statistics M49 Codes)
 		/// </summary>
-		public string ISO3166_1_Num { get; private set; }
+		public string IsoNum { get; private set; }
 		/// <summary>
 		/// Codes assigned by the International Telecommunications Union
 		/// </summary>
-		public string ITU { get; private set; }
+		public string Itu { get; private set; }
 		/// <summary>
 		/// MAchine-Readable Cataloging codes from the Library of Congress
 		/// </summary>
-		public string MARC { get; private set; }
+		public string Marc { get; private set; }
 		/// <summary>
 		/// Country abbreviations by the World Meteorological Organization
 		/// </summary>
-		public string WMO { get; private set; }
+		public string Wmo { get; private set; }
 		/// <summary>
 		/// Distinguishing signs of vehicles in international traffic
 		/// </summary>
-		public string DS { get; private set; }
+		public string Ds { get; private set; }
 		/// <summary>
 		/// Country code from ITU-T recommendation E.164, sometimes followed by area code
 		/// </summary>
@@ -52,27 +52,27 @@ namespace HermaFx.Globalization
 		/// <summary>
 		/// Codes from the U.S. standard FIPS PUB 10-4
 		/// </summary>
-		public string FIPS { get; private set; }
+		public string Fips { get; private set; }
 		/// <summary>
 		/// ISO 4217 currency alphabetic code
 		/// </summary>
-		public string ISO4217_Currency_Alphabetic { get; private set; }
+		public string CurrencyAlphabetic { get; private set; }
 		/// <summary>
 		/// ISO 4217 country name
 		/// </summary>
-		public string ISO4217_Currency_Country { get; private set; }
+		public string CurrencyCountry { get; private set; }
 		/// <summary>
 		/// ISO 4217 currency number of minor units
 		/// </summary>
-		public string ISO4217_Currency_Minor_Unit { get; private set; }
+		public string CurrencyMinorUnit { get; private set; }
 		/// <summary>
 		/// ISO 4217 currency name
 		/// </summary>
-		public string ISO4217_Currency_Name { get; private set; }
+		public string CurrencyName { get; private set; }
 		/// <summary>
 		/// ISO 4217 currency numeric code
 		/// </summary>
-		public string ISO4217_Currency_Numeric { get; private set; }
+		public string CurrencyNumeric { get; private set; }
 		/// <summary>
 		/// Country status, based on the CIA World Factbook
 		/// </summary>
@@ -88,54 +88,55 @@ namespace HermaFx.Globalization
 		/// <summary>
 		/// Top level domain from Geonames
 		/// </summary>
-		public string TLD { get; private set; }
+		public string Tld { get; private set; }
 		/// <summary>
 		/// Languages from Geonames
 		/// </summary>
-		public string Languages { get; private set; }
+		public string[] Languages { get; private set; }
 		/// <summary>
 		/// Indicates if the country is a member of the European Union
 		/// </summary>
-		public bool IsMemberOfEU { get; private set; }
+		/// <seealso cref="http://europa.eu/about-eu/countries/index_en.htm"/>
+		public bool IsMemberOfEu { get; private set; }
 
 		public string Name => _name();
-		public string Official_Name_EN => _official_Name_EN();
+		public string OfficialNameEn => _officialNameEn();
 		#endregion
 
 		#region .ctor
-		public Country(string iso3166_1_alpha3, Func<string> name, Func<string> official_name_en, string iso3166_1_alpha2, string iso3166_1_num,
+		public Country(string iso3, Func<string> name, Func<string> officialNameEn, string iso2, string isoNum,
 			string itu, string marc, string wmo, string ds, string dial, string fips,
-			string iso4217_currency_alphabetic, string iso4217_currency_country, string iso4217_currency_minor_unit,
-			string iso4217_currency_name, string iso4217_currency_numeric,
-			string isIndependent, string capital, string continent, string tld, string languages, bool isMemberOfEu)
+			string currencyAlphabetic, string currencyCountry, string currencyMinorUnit,
+			string currencyName, string currencyNumeric,
+			string isIndependent, string capital, string continent, string tld, string[] languages, bool isMemberOfEu)
 		{
-			Guard.IsNotNullNorWhitespace(iso3166_1_alpha3, nameof(iso3166_1_alpha3));
+			Guard.IsNotNullNorWhitespace(iso3, nameof(iso3));
 			Guard.IsNotNull(name, nameof(name));
-			Guard.IsNotNullNorWhitespace(iso3166_1_alpha2, nameof(iso3166_1_alpha2));
+			Guard.IsNotNullNorWhitespace(iso2, nameof(iso2));
 			Guard.IsNotNullNorWhitespace(fips, nameof(fips));
 
-			ISO3166_1_Alpha3 = iso3166_1_alpha3;
+			Iso3 = iso3;
 			_name = name;
-			_official_Name_EN = official_name_en;
-			ISO3166_1_Alpha2= iso3166_1_alpha2;
-			ISO3166_1_Num = iso3166_1_num;
-			ITU = itu;
-			MARC = marc;
-			WMO = wmo;
-			DS = ds;
+			_officialNameEn = officialNameEn;
+			Iso2= iso2;
+			IsoNum = isoNum;
+			Itu = itu;
+			Marc = marc;
+			Wmo = wmo;
+			Ds = ds;
 			Dial = dial;
-			FIPS = fips;
-			ISO4217_Currency_Alphabetic = iso4217_currency_alphabetic;
-			ISO4217_Currency_Country = iso4217_currency_country;
-			ISO4217_Currency_Minor_Unit = iso4217_currency_minor_unit;
-			ISO4217_Currency_Name = iso4217_currency_name;
-			ISO4217_Currency_Numeric = iso4217_currency_numeric;
+			Fips = fips;
+			CurrencyAlphabetic = currencyAlphabetic;
+			CurrencyCountry = currencyCountry;
+			CurrencyMinorUnit = currencyMinorUnit;
+			CurrencyName = currencyName;
+			CurrencyNumeric = currencyNumeric;
 			IsIndependent = isIndependent;
 			Capital = capital;
 			Continent = continent;
-			TLD = tld;
+			Tld = tld;
 			Languages = languages;
-			IsMemberOfEU = isMemberOfEu;
+			IsMemberOfEu = isMemberOfEu;
 		}
 		#endregion
 	}
