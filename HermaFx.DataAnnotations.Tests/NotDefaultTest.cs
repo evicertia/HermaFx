10db98569d8561fcbd775053b3bc82927afc38c0
Danilo Olivares <dolivares@evicertia.com>
2016-10-06 14:17:37 +0200

    add NotDefaultTest

---
+++ HermaFx.DataAnnotations.Tests/NotDefaultTest.cs
@@ -0,0 +1,38 @@
+﻿
+using NUnit.Framework;
+using System;
+
+namespace HermaFx.DataAnnotations
+{
+	[TestFixture]
+	public class NotDefaultTest
+	{
+		private class Model
+		{
+			[NotDefault]
+			public Guid Value { get; set; }
+		}
+
+		[Test]
+		public void IsValid()
+		{
+			var model = new Model() { Value = Guid.NewGuid() };
+			ExtendedValidator.EnsureIsValid(model);
+		}
+
+		[Test]
+		public void IsNotValid()
+		{
+			var model = new Model() { Value = default(Guid) };
+			Assert.Throws<AggregateValidationException>(() => ExtendedValidator.EnsureIsValid(model));
+		}
+
+		[Test]
+		public void IsNotValidWithNulls()
+		{
+			var model = new Model() { };
+			Assert.Throws<AggregateValidationException>(() => ExtendedValidator.EnsureIsValid(model));
+		}
+
+	}
+}
