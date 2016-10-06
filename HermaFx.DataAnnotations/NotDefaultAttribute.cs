532e3085520d5a13df7caec52f7f3f51f762507f
Danilo Olivares <dolivares@evicertia.com>
2016-10-06 14:16:03 +0200

    add new GetDefault() method and use it instead of GetDefaultValue

--- HermaFx.DataAnnotations/NotDefaultAttribute.cs
+++ HermaFx.DataAnnotations/NotDefaultAttribute.cs
@@ -11,17 +11,21 @@ namespace HermaFx.DataAnnotations
 	{
 		private const string _defaultErrorMessage = "The field {0} requires a non-default value";
 
-		public NotDefaultAttribute()
-				: base(string.Format(_defaultErrorMessage))
-		{
-		}
-
 		public override bool IsValid(object value)
 		{
 			if (value == null) return true;
 			if (!value.GetType().IsValueType) return true;
 
-			return value == value.GetType().GetDefaultValue();
+			return !object.Equals(value, value.GetType().GetDefault());
 		}
+
+		public override string FormatErrorMessage(string name)
+		{
+			if (string.IsNullOrEmpty(ErrorMessageResourceName) && string.IsNullOrEmpty(ErrorMessage))
+				ErrorMessage = _defaultErrorMessage;
+
+			return string.Format(ErrorMessageString, name);
+		}
+
 	}
 }
--- HermaFx.Foundation/RuntimeExtensions/TypeExtensions.cs
+++ HermaFx.Foundation/RuntimeExtensions/TypeExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Concurrent;
 
 namespace HermaFx
@@ -8,9 +9,39 @@ namespace HermaFx
 		//a thread-safe way to hold default instances created at run-time
 		private static ConcurrentDictionary<Type, object> typeDefaults = new ConcurrentDictionary<Type, object>();
 
-		public static object GetDefaultValue(this Type type)
+		public static object GetDefault(this Type type)
 		{
-			return type.IsValueType ? typeDefaults.GetOrAdd(type, t => Activator.CreateInstance(t)) : null;
+			Guard.IsNotNull(type, nameof(type));
+
+			// If no Type was supplied, if the Type was a reference type, or if the Type was a System.Void, return null
+			if (type == null || !type.IsValueType || type == typeof(void))
+				return null;
+
+			// If the supplied Type has generic parameters, its default value cannot be determined
+			if (type.ContainsGenericParameters)
+			{
+				throw new ArgumentException($"The supplied value type <{type}> contains generic parameters, so the default value cannot be retrieved");
+			}
+
+			// If the Type is a primitive type, or if it is another publicly-visible value type (i.e. struct), return a 
+			//  default instance of the value type
+			if (type.IsPrimitive || !type.IsNotPublic)
+			{
+				try
+				{
+					return typeDefaults.GetOrAdd(type, t => Activator.CreateInstance(t));
+				}
+				catch (Exception e)
+				{
+					throw new ArgumentException(
+					$"The Activator.CreateInstance method could not create a default instance of the supplied value type <{type}>\n\n"+
+					$"Exception message:{e.Message}", e);
+				}
+			}
+
+			// Fail with exception
+			throw new ArgumentException($"The supplied value type <{type}> is not a publicly-visible type, so the default value cannot be retrieved");
 		}
+
 	}
 }
