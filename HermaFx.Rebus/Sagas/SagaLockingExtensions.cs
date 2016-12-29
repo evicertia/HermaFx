06463ab0eca5a6688fe5d6fa4f9785151c3bc343
David Bueno <dbueno@evicertia.com>
2016-12-29 16:37:28 +0100

    Added description for RebusSagaLockingExtension

--- HermaFx.Rebus/Sagas/SagaLockingExtensions.cs
+++ HermaFx.Rebus/Sagas/SagaLockingExtensions.cs
@@ -11,6 +11,13 @@ namespace HermaFx.Rebus.Sagas
 {
 	public static class SagaLockingExtensions
 	{
+		/// <summary>
+		/// Extension to allow saga locking while handling messages.
+		/// </summary>
+		/// <param name="configurer">The own RebusSagaConfigurer instance</param>
+		/// <param name="provider">ISagaLockingProvider instance implementation</param>
+		/// <param name="timeout">Nullable Timespan to allow lock retry until TimeOut reached</param>
+		/// <returns>The own RebusSagaConfigurer Instance</returns>
 		public static RebusSagasConfigurer WithSagaLocking(this RebusSagasConfigurer configurer, ISagaLockingProvider provider
 			, TimeSpan? timeout = null)
 		{
