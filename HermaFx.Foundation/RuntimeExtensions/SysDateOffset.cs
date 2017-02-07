3c7e152831eb44394d8fce991c4cc9de792b4f17
David Bueno <dbueno@evicertia.com>
2017-02-07 17:29:07 +0100

    Added SysDateOffset to RuntimeExtensions to allow mocking of a DateTimeOffset

---
+++ HermaFx.Foundation/RuntimeExtensions/SysDateOffset.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HermaFx
+{
+	public static class SysDateOffset
+	{
+		public static Func<DateTimeOffset> NowDelegate { get; set; }
+		public static Func<DateTimeOffset> UtcNowDelegate { get; set; }
+
+		public static DateTimeOffset Now
+		{
+			get
+			{
+				if (NowDelegate == null)
+					return DateTimeOffset.Now;
+
+				return NowDelegate();
+			}
+		}
+
+		public static DateTimeOffset UtcNow
+		{
+			get
+			{
+				if (UtcNowDelegate == null)
+					return DateTimeOffset.UtcNow;
+
+				return UtcNowDelegate();
+			}
+		}
+	}
+}
