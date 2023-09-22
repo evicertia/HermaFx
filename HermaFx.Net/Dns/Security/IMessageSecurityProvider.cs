955647b65f8397b275dbc16373dcb5d22828304f
David Bueno <david@evicertia.com>
2023-09-22 18:17:48 +0200

    Added DnDns library with custom modification for DnsQueryRequest to avoid query hostentry when IPAddress is facilitated at DnsQueryRequest

---
+++ HermaFx.Net/Dns/Security/IMessageSecurityProvider.cs
@@ -0,0 +1,9 @@
+﻿using HermaFx.Net.Dns.Query;
+
+namespace HermaFx.Net.Dns.Security
+{
+	public interface IMessageSecurityProvider
+	{
+		DnsQueryRequest SecureMessage(DnsQueryRequest dnsQueryRequest);
+	}
+}
