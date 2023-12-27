7f92c81724969d5309e203f3cfde5fa9f3bbf965
fravalpin <fvallepinar@gmail.com>
2023-12-27 10:47:19 +0100

    Created DistinctElementsAttribute

---
+++ HermaFx.DataAnnotations.Tests/DistinctElementsAttributeTest.cs
@@ -0,0 +1,84 @@
+﻿
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace HermaFx.DataAnnotations
+{
+	[TestFixture]
+	public class DistinctElementsAttributeTest
+	{
+
+		private class Model
+		{
+			#region Innter Type
+			public class _Item
+			{
+				public string Property1 { get; set; }
+				public string Property2 { get; set; }
+
+			}
+			#endregion
+
+			[DistinctElements("Property1")]
+			public IEnumerable<_Item> Values { get; set; }
+
+		}
+
+		[Test]
+		public void IsValidTest()
+		{
+			var model = new Model();
+
+			model.Values = new List<Model._Item>()
+			{
+					new Model._Item() { Property1 = "hello", Property2 = "world" },
+					new Model._Item() { Property1 = "hi", Property2 = "world" },
+			};
+
+			Assert.That(ExtendedValidator.IsValid(model), Is.True);
+		}
+
+		[Test]
+		public void IsNotValidTest()
+		{
+			var model = new Model();
+
+			model.Values = new List<Model._Item>()
+			{
+					new Model._Item() { Property1 = "hello", Property2 = "world" },
+					new Model._Item() { Property1 = "hello", Property2 = "world" },
+			};
+
+			Assert.That(ExtendedValidator.IsValid(model), Is.False);
+		}
+
+		[Test]
+		public void IsValidWithNullTest()
+		{
+			var model = new Model();
+
+			model.Values = new List<Model._Item>()
+			{
+					new Model._Item() { Property1 = "hello", Property2 = "world" },
+					new Model._Item() { Property1 = null, Property2 = "world" },
+			};
+
+			Assert.That(ExtendedValidator.IsValid(model), Is.True);
+		}
+
+		[Test]
+		public void IsNotValidWithNullTest()
+		{
+			var model = new Model();
+
+			model.Values = new List<Model._Item>()
+			{
+					new Model._Item() { Property1 = null, Property2 = "world" },
+					new Model._Item() { Property1 = null, Property2 = "world" },
+			};
+
+			Assert.That(ExtendedValidator.IsValid(model), Is.False);
+		}
+
+	}
+}
---
+++ HermaFx.DataAnnotations/DistinctElementsAttribute.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace HermaFx.DataAnnotations
+{
+	[AttributeUsage(AttributeTargets.Property)]
+	public class DistinctElementsAttribute : ValidationAttribute
+	{
+		public string UniqueProperty { get; }
+		public Type ElementType { get; }
+		public DistinctElementsAttribute(string property)
+		{
+			ErrorMessage = "The property {0} must be unique in the collection.";
+			UniqueProperty = property;
+		}
+
+		private bool IsGenericEnumerable(Type type)
+		{
+			return type.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>));
+		}
+
+		private IEnumerable<Type> GetGenericEnumerableTypes(Type type)
+		{
+			return type.GetInterfaces().Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>)).ToArray();
+		}
+
+		protected override ValidationResult IsValid(object value, ValidationContext context)
+		{
+			if (value is null)
+			{
+				return ValidationResult.Success;
+			}
+			// If value is IEnumerate, check if the property being validate is unique in the collection
+			else if (!IsGenericEnumerable(value.GetType()))
+			{
+				throw new InvalidOperationException("The property being validated must be an IEnumerable.");
+			}
+
+			var ifaces = GetGenericEnumerableTypes(value.GetType())
+				.Where(x => ElementType == null || x.GetGenericArguments()[0].GetType() == ElementType);
+
+			if (ifaces.Count() > 1)
+			{
+				throw new InvalidOperationException("The property being validated implements more than one generic IEnumerable<>, please use ElementType to indicate intended type.");
+			}
+			else if (ifaces.Count() == 0)
+			{
+				throw new InvalidOperationException("Unable to guess element type, maybe an incorrect ElementType was specified?");
+			}
+
+			var type = ElementType ?? ifaces.First().GetGenericArguments()[0];
+
+			Guard.Against<InvalidOperationException>(
+				type.GetProperty(UniqueProperty) == null,
+				"ElementType ({0}) does not contains a property named {1}?", type, UniqueProperty
+			);
+
+			var collection = ((IEnumerable)value).Cast<object>().ToArray();
+			var property = type.GetProperty(UniqueProperty);
+
+			if (collection.Select(x => property.GetValue(x)).Distinct().Count() == collection.Count())
+			{
+				return ValidationResult.Success;
+			}
+
+			return new ValidationResult(FormatErrorMessage(UniqueProperty));
+		}
+	}
+}
